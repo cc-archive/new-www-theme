b9c464f46955dd789a264c97f954ef52f08d259e
Floyd Mann <floyd@affinitybridge.com>
2016-05-27 10:33:54 -0700

    refs #180. Linking profile image to author profile.

--- cc/template-parts/content-single.php
+++ cc/template-parts/content-single.php
@@ -19,7 +19,7 @@
 		<div class="author-image">
 	  <?php
 	    $author_bio_avatar_size = apply_filters( 'twentysixteen_author_bio_avatar_size', 50 );
-	    echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
+	    echo '<a href="' . get_author_posts_url(get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' )) . '">' . get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size ) . '</a>';
 	    ?>
 		</div>
 	  <div class="author-info-group">
