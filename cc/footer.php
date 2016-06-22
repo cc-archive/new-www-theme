0b4d0da3ac248fcd9b02812f31d7ec5383a73916
Dylan McCall <dylanmccall@gmail.com>
2016-06-21 17:35:43 -0700

    Change "//creativecommons.org" to "https://"
    This site is consistently served over https, and protocol-relative links don't really belong here.

--- cc/footer.php
+++ cc/footer.php
@@ -50,14 +50,14 @@
 
           <div class="column cc-footer-license">
             <div class="license-icons">
-              <a rel="license" href="//creativecommons.org/licenses/by/4.0/" title="Creative Commons Attribution 4.0 International license">
+              <a rel="license" href="https://creativecommons.org/licenses/by/4.0/" title="Creative Commons Attribution 4.0 International license">
                 <?php /* <img src="https://licensebuttons.net/l/by/4.0/88x31.png" alt="License"> */ ?>
                 <i class="cc-icon-cc"></i><i class="cc-icon-cc-by"></i>
               </a>
             </div>
             <aside>
-              <div xmlns:cc="//creativecommons.org/ns#" about="//creativecommons.org">
-                <p>Except where otherwise <a class="subfoot" href="/policies#license">noted</a>, content on this site is licensed under a <a class="subfoot" href="//creativecommons.org/licenses/by/4.0/" rel="license">Creative Commons Attribution 4.0 International license</a>. <a class="subfoot" href="/website-icons" target="blank">Icons</a> by The Noun Project.</p>
+              <div xmlns:cc="https://creativecommons.org/ns#" about="https://creativecommons.org">
+                <p>Except where otherwise <a class="subfoot" href="/policies#license">noted</a>, content on this site is licensed under a <a class="subfoot" href="https://creativecommons.org/licenses/by/4.0/" rel="license">Creative Commons Attribution 4.0 International license</a>. <a class="subfoot" href="/website-icons" target="blank">Icons</a> by The Noun Project.</p>
               </div>
               <p class="website-credit">Website by <a class="subfoot" href="http://affinitybridge.com/" target="blank"> Affinity Bridge</a>.</p>
             </aside>
