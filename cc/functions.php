39fad0d938e9a95dbc8110dc4aa61777b6b89cde
Hugo Solar <hugo.solar@gmail.com>
2019-12-16 13:33:14 -0300

    fix #creativecommons/creativecommons.org#1056 - add google tag manager script to website footer

--- cc/functions.php
+++ cc/functions.php
@@ -301,4 +301,34 @@ add_filter( 'gform_require_login_pre_download', '__return_true' );
 // Akismet is too eager to mark these as spam - RobM
 add_filter( 'gform_akismet_enabled_17' , '__return_false' );
 
+add_action( 'wp_footer', 'cc_add_google_marketing_script' );
+
+function cc_add_google_marketing_script() {
+  ?>
+  <script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
+
+      __gaTracker('create', 'UA-2010376-1', 'auto');
+      __gaTracker('set', 'forceSSL', true);
+      __gaTracker('send','pageview');
+  </script>
+  <!-- Google Tag Manager -->
+  <script type="text/javascript">
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KP4CRCM');
+  </script>
+  <!-- End Google Tag Manager -->
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KP4CRCM"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <?php
+}
+
 ?>
