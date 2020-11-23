2f9d788d29e7e1ea907e1145f0bad3eafd3b3f9d
Hugo Solar <hugo.solar@gmail.com>
2020-11-23 12:05:32 -0300

    add tag manager script to header

--- cc/functions.php
+++ cc/functions.php
@@ -344,13 +344,20 @@ function get_web_monetization_tags() {
   }
 }
 
-add_action( 'wp_footer', 'cc_add_google_marketing_script' );
-
-function cc_add_google_marketing_script() {
-  ?>
+add_action( 'wp_footer', 'cc_add_google_marketing_script_footer' );
+add_action( 'wp_head', 'cc_add_google_marketing_script_header' );
+function cc_add_google_marketing_script_header() {
+?>
+<!-- Google Tag Optimize -->
+  <script src="https://www.googleoptimize.com/optimize.js?id=OPT-K7PLVT5"></script> 
+  <!-- End Google Optimize -->
   <!-- Google Tag Manager -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5F5HKGX');</script>
   <!-- End Google Tag Manager -->
+<?php
+}
+function cc_add_google_marketing_script_footer() {
+  ?>
   <!-- Google Tag Manager (noscript) -->
   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F5HKGX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
